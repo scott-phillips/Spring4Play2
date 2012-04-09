6d41d68d7f29db95b3fe690e320e099ec8b77a60
Scott Phillips <scott@scottphillips.com>
2012-04-09 15:19:21 -0500

    Fixed project name is the build files

--- samples/AnnotationConfigExample/project/Build.scala
+++ samples/AnnotationConfigExample/project/Build.scala
@@ -4,7 +4,7 @@ import PlayProject._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "ComponentScanningExample"
+    val appName         = "AnnotationConfigExample"
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
--- samples/BasicSpringExample/project/Build.scala
+++ samples/BasicSpringExample/project/Build.scala
@@ -4,7 +4,7 @@ import PlayProject._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "SpringSampleProject"
+    val appName         = "BasicSpringExample"
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
